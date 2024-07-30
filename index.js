7714af9d2c0cad3755dd34bc1767ca10a98661fa
Adithya2020 <adithya16.ak@gmail.com>
2024-07-30 15:28:32 +0530

    for loops

---
+++ index.js
@@ -0,0 +1,18 @@
+
+for (let i = 1; i <= 5; i++) {
+    let stars = "";
+    for (let j = 1; j <= i; j++) {
+      stars += "*";
+    }
+    console.log(stars);
+  }
+
+for (let x = 1; x <= 5; x++){
+    let stars = "";
+    for (let y=5; y>=x; y--){
+        stars +="*";
+    }
+    console.log(stars);
+
+}
+
