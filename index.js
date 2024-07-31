bb38ef4ebf5bf1d1b6a007d1fee510f234c9f552
srinidhi-gayatri <srinidhigayatri6@gmail.com>
2024-07-31 11:08:42 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log("hello world...!");
+console.log("hello world...!",3,4,"i am learning javascript");
+let x=10;
+let y=15;
+console.log(x+y);
+console.log(x-y);
+let sri_vsk=("it is a string called vsk and sri");
+let Name="sri";
+let NaMe="vsk";
+console.log("sri_vsk");
+console.log("sri");
+console.log("vsk");
+
+let age=21;
+if (age>=18) {
+    console.log("eligible to vote");
+}else{
+    console.log("not eligible to vote");
+}
+
+let num=18;
+if (num<=18) {
+    console.log("it is a number");
+}
+
+let a=25;
+console.log(a);
