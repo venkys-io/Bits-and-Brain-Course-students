dbf1ed7896b24cc06f368d04719561788c0b585e
srinidhi-gayatri <srinidhigayatri6@gmail.com>
2024-07-31 19:29:15 +0530

    Add files via upload

---
+++ cse.java
@@ -0,0 +1,19 @@
+class cse_1 {
+  public void srinidhi() {
+ System.out.println("ViJUULU");
+  }
+ public void vsk() {
+       System.out.println("SHINE");
+   }
+    public void chandrika() {
+        System.out.println("BITTU");
+   }
+}
+    public class CSE {
+        public static void main(String[] args) {
+            cse_1 obj=new cse_1();
+            obj.srinidhi();
+            obj.vsk();
+            obj.chandrika();
+        }
+    } 
