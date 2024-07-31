2bc4209e6d7356240387962d395a09eb770ffce6
sarath-chandrika <chsarathchandrika@gmail.com>
2024-07-31 11:31:40 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+console.log("This is chandrika");
+let x = 10;
+let y = 20;
+console.log(x + y);
+console.log(x - y);
+let sarath=("SARATH CHANDRIKA");
+console.log(sarath);
+let sarath_chandrika=("it is a string called sarath and chandrika");
+console.log("sarath_chandrika");
+let age=21;
+if(age>=18) {
+console.log("eligible to vote");
+}
+else {
+    console.log("not eligible to vote");
+}
+let a=17;
+console.log(a);
