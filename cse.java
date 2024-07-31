d1be65d4447d68b3f7ac0fb3672b98693882342d
sarath-chandrika <chsarathchandrika@gmail.com>
2024-07-31 20:11:48 +0530

    Add files via upload

---
+++ cse.java
@@ -0,0 +1,19 @@
+class cse_1{
+    public void Chandrika(){
+        System.out.println("Hello");
+    }
+public void Srinidhi(){
+    System.out.println("Gayatri");
+}
+public void Adithya(){
+    System.out.println("how are you?");
+}
+}
+public class cse {
+    public static void main(String[] args) {
+        cse_1 obj=new cse_1();
+        obj.Chandrika();
+        obj.Srinidhi();
+        obj.Adithya();
+    }
+}
