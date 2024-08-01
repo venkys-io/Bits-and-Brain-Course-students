2afb7c45dcafa28b3a15905e5a2af846bc1c43ae
sarath-chandrika <chsarathchandrika@gmail.com>
2024-08-01 16:59:36 +0530

    Add files via upload

---
+++ aoc.java
@@ -0,0 +1,10 @@
+public class aoc {
+
+    public static void main(String[] args) {
+        int num = 20;
+       // int num1=10;
+       // int result = num % num1;
+       int result = num + 1;
+        System.out.println(result);
+    }
+}
