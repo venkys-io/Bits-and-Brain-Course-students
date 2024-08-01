8414e80747cf156409260750d069db64ce7a723c
sarath-chandrika <chsarathchandrika@gmail.com>
2024-08-01 17:00:18 +0530

    Add files via upload

---
+++ lo.java
@@ -0,0 +1,13 @@
+public class lo {
+    public static void main(String[] args) {
+        boolean a=true;
+        boolean b=false;
+        int age=27;
+        boolean hasDrivingLicense=true;
+        boolean isEligibleForDriving=(age>=18) && hasDrivingLicense;
+        System.out.println(isEligibleForDriving);
+        int score=70;
+        boolean passedExam=(score>=20) || (score<80 && hasDrivingLicense);
+        System.out.println(passedExam);
+    }
+}
