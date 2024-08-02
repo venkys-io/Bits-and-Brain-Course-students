c2b702e62620c02d6a36d7d4d31adf2bbf1f3a5f
sarath-chandrika <chsarathchandrika@gmail.com>
2024-08-02 20:34:35 +0530

    Add files via upload
    Few conditional statements such as if , else ,else if and switch statement

---
+++ controlstatements.java
@@ -0,0 +1,79 @@
+public class controlstatements {
+public static void main(String[] args) {
+// int num = 10;
+  //if (true) {
+// System.out.println("the number is positive");
+//}
+//if (num>5) {
+ // System.out.println("the number is positive");
+//}
+//int x = 5;
+//int y = 7;
+//if (x > y) {
+ // System.out.println("the maximum is x");
+//}
+//if (y > x ) {
+ // System.out.println("the maximum is y");
+//}
+//int a = 3;
+//if (a>5) {
+  //System.out.println("a is greater than 5");
+  
+//}else if (a>2) {
+  //System.out.println("a is greater than 2");
+  //int day = 3;
+  //if(day==1){
+    // System.out.println("Monday");
+    // }else if (day==2){
+    // System.out.println("Tuesday");
+    // }else if (day==3){
+    // System.out.println("Wednesday");
+    // }else if (day==4){
+    // System.out.println("Thursday");
+    // }else if (day==5){
+    // System.out.println("Friday");
+    // }else if (day==6){
+    // System.out.println("Saturday");
+    // }else if (day==7){
+    // System.out.println("Sunday");
+    // }
+// switch
+//int month = 3;
+//switch (month) {
+  //case 1:
+ // System.out.println("january");
+  //break;
+  //case 2: 
+  //System.out.println("february");
+  //break;
+  //case 3:
+  //System.out.println("march");
+  //break;
+  //case 4:
+  //System.out.println("april");
+  //case 5:
+  //System.out.println("may");
+  //break;
+//default:System.out.println("enter a valid month");
+int[] numbers = { 2,4,6,8};
+//System.out.println(numbers[3]);
+//if (i == 3) {
+  // // System.out.println("In the continue");
+  // //break;
+  // continue;
+  for (int number : numbers) {
+  System.out.println(number);
+}
+
+  
+}
+
+}
+
+
+
+
+
+  
+
+
