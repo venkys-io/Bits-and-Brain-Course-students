987f4598fd4bec2275a0431de3d00575fc17660a
sarath-chandrika <chsarathchandrika@gmail.com>
2024-08-10 19:57:25 +0530

    Add files via upload

---
+++ Strings.java
@@ -0,0 +1,16 @@
+public class Strings {
+    public static void main(String[] args) {
+       // String str1 = "chandrika";
+        //String str2 = new String("chandrika");
+        //System.out.println(str1==str2);
+        //PublicExample obj = new PublicExample();
+         //obj.number = 25;
+         obj.display();
+         Child obj = new Child();
+        obj.show();
+        String s1="chandrika";
+
+        
+}
+}
+
